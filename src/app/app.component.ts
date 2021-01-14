96f18da2d9727ba8094af79cece71feb6e382f57
cesar <cesitarxedlol@gmail.com>
2021-01-14 13:21:41 +0100

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularForm';
+
+  emailCorreo: FormControl;
+
+  constructor(){
+    this.emailCorreo= new FormControl('', [
+      Validators.required,
+      Validators.maxLength(3)
+    ]);
+    this.emailCorreo.valueChanges
+    .subscribe(value=>{
+      console.log(value)
+    })
+  }
 }
+
